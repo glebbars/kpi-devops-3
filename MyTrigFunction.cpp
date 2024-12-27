59399aee20d505c8bd64ec5c170e937ca545afbd
glebbars <glebars2004@gmail.com>
2024-12-27 19:45:31 +0000

    feat(`FuncA`): calculate the sum of the first 3 elements of an infinite sequence

--- MyTrigFunction.cpp
+++ MyTrigFunction.cpp
@@ -7,11 +7,11 @@ double MyTrigFunction::FuncA() {
     double x = 1.0;
     double sum = 0.0;
 
-    for (int i = 0; i < 1; ++i) {
+    for (int i = 0; i < 3; ++i) {
         double numerator = (i % 2 == 0 ? 1.0 : -1.0) * tgamma(2 * i + 1);
         double denominator = pow(4, i) * pow(tgamma(i + 1), 2);
         sum += (numerator / denominator) * pow(x, i);
     }
 
-    return 0;
+    return sum;
 }
